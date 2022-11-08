5eda705cf0127cdaaed9d155f3883d34efa96d35
Ninja <cuansenputra@gmail.com>
2022-11-08 17:06:48 +0700

    Homepage blog and fetch JSON data

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,47 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import BlogLists from "./BlogLists";
+
 
 const Home = () => {
-    const [name, setName] = useState('Mario')
-    const [age, setAge] = useState(17)
-
-    const handleClick = () => {
-        setName('Luigi')
-        setAge(19)
-    }
+    // const [blogs, setBlogs] = useState([
+    //     { title : ' Moyi dan Miyi tebang', body : ' none ', author : 'odo', id : '1'},
+    //     { title : ' odo dan Miyi tebang', body : ' none ', author : 'igy', id : 2},
+    //     { title : ' Moyi dan odo tebang', body : ' none ', author : 'moyi', id : 3}
+    // ])
+    const [blogs, setBlogs] = useState(null)
+    const [isPending, setPending] = useState(true)
+    const [error, setError] = useState(null)
     
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then((res) => {
+                if(!res.ok) throw Error('Could not fetch the data from the resource')
+                return res.json()
+            })
+            .then((data) => {
+                setBlogs(data)
+                setPending(false)
+                setError(null)
+            })
+            .catch((err) => {
+                setPending(false)
+                setError(err.message)
+            })
+        },1000);
+    },[])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlogs)
+        console.log(newBlogs)
+    }
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>Hallo, my name's {name} and I'm {age}</p>
-            <button onClick={ handleClick }>Click Me</button>
+            {error && <div>{error}</div> }
+            {isPending && <div>Loading...</div> }
+            {blogs && <BlogLists blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+            {/* <BlogLists blogs={blogs.filter((blog) => blog.Author === 'moyi')} title="Moyi's Blogs"/> */}
         </div>
       );
 }
